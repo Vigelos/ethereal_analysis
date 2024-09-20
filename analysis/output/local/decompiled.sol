74085e006d580fdc38e4b87cdbc348d9c6797dbd
Vigelos Cromwell <vigelos@Vigeloss-MacBook-Pro.local>
2024-09-20 03:57:10 +0100

    add example

--- analysis/output/local/decompiled.sol
+++ analysis/output/local/decompiled.sol
@@ -17,58 +17,65 @@ pragma solidity >=0.8.0;
 contract DecompiledContract {
     
     
-    /// @custom:selector    0x11479fea
-    /// @custom:signature   Unresolved_11479fea(uint256 arg0, uint256 arg1, uint256 arg2, uint256 arg3) public payable returns (uint256)
+    /// @custom:selector    0x43753b4d
+    /// @custom:signature   Unresolved_43753b4d(uint256 arg0, uint256 arg1, uint256 arg2, uint256 arg3, uint256 arg4, uint256 arg5, uint256 arg6, uint256 arg7, uint256 arg8) public payable returns (uint256)
+    /// @param              arg0 ["uint256", "bytes32", "int256"]
+    /// @param              arg1 ["uint256", "bytes32", "int256"]
+    /// @param              arg2 ["uint256", "bytes32", "int256"]
+    /// @param              arg3 ["uint256", "bytes32", "int256"]
+    /// @param              arg4 ["uint256", "bytes32", "int256"]
+    /// @param              arg5 ["uint256", "bytes32", "int256"]
+    /// @param              arg6 ["uint256", "bytes32", "int256"]
+    /// @param              arg7 ["uint256", "bytes32", "int256"]
     /// @param              arg8 ["uint256", "bytes32", "int256"]
-    /// @param              arg9 ["uint256", "bytes32", "int256"]
-    /// @param              arg10 ["uint256", "bytes32", "int256"]
-    /// @param              arg11 ["uint256", "bytes32", "int256"]
-    function Unresolved_11479fea(uint256 arg0, uint256 arg1, uint256 arg2, uint256 arg3) public payable returns (uint256) {
-        require(msg.value);
-        require(0x44 > msg.data.length);
-        require(0xc4 > msg.data.length);
-        require(0x0104 > msg.data.length);
-        require(0x0164 > msg.data.length);
+    function Unresolved_43753b4d(uint256 arg0, uint256 arg1, uint256 arg2, uint256 arg3, uint256 arg4, uint256 arg5, uint256 arg6, uint256 arg7, uint256 arg8) public payable returns (uint256) {
+        require(!msg.data.length < 0x44);
+        require(!0xc4 > msg.data.length);
+        require(!msg.data.length < 0x0104);
+        require(!0x0124 > msg.data.length);
         uint256 var_a = var_a + 0x0380;
-        require(!0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 > arg8);
+        require(arg8 < 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47);
         uint256 var_b = 0;
         return 0;
-        require(!0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 > arg9);
-        var_b = 0;
-        return 0;
-        require(!0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 > arg10);
-        var_b = 0;
-        return 0;
-        require(!0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 > arg11);
-        var_b = 0;
-        return 0;
-        var_c = 0x2c235c58c31ea661cda842be40babc5f55216450cdc2ba3e7cfdd0c38726ad92;
-        var_d = 0x07b664f0c88018661cce182aef0362013b398927fd1b608e5d2de1cf68f9b95b;
-        var_e = 0x274bd7231500754153d2c36b404f3806d27ee83d9f3929be6c0b05cac5d090bd;
-        var_f = 0x1bebd67c2ab80ed58474ffcd173fb20bfcd15094410a5e8c0c948af57ceaeb1e;
+        var_c = 0x128e5e76fadfbd446d09b5f55d1b04955a7627f7011ee37aa95e1b8e434f500e;
+        var_d = 0x16bf8f80f8119bf7173d578cb9a47c6b877aae9c1ca0d4136022465648a9a384;
+        var_e = 0x2becb10d5facc91b86559d5a29674b4aed879adfd0e09c8574f3a7193447a465;
+        var_f = 0x0d61127e88c3159b81e486b0203a366bba20dca4c7cf6f820f3fde4b55eaa28a;
         uint256 var_g = arg8;
-        (bool success, bytes memory ret0) = address(0x07).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x274bd7231500754153d2c36b404f3806d27ee83d9f3929be6c0b05cac5d090bd, 0x1bebd67c2ab80ed58474ffcd173fb20bfcd15094410a5e8c0c948af57ceaeb1e, arg8));
+        (bool success, bytes memory ret0) = address(0x07).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x2becb10d5facc91b86559d5a29674b4aed879adfd0e09c8574f3a7193447a465, 0x0d61127e88c3159b81e486b0203a366bba20dca4c7cf6f820f3fde4b55eaa28a, arg8));
         var_b = 0;
         return 0;
-        var_g = var_h;
-        var_i = var_j;
-        (bool success, bytes memory ret0) = address(0x06).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x274bd7231500754153d2c36b404f3806d27ee83d9f3929be6c0b05cac5d090bd, 0x1bebd67c2ab80ed58474ffcd173fb20bfcd15094410a5e8c0c948af57ceaeb1e, var_h, var_j));
+        var_g = var_a.length;
+        var_h = var_i;
+        (bool success, bytes memory ret0) = address(0x06).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x2becb10d5facc91b86559d5a29674b4aed879adfd0e09c8574f3a7193447a465, 0x0d61127e88c3159b81e486b0203a366bba20dca4c7cf6f820f3fde4b55eaa28a, var_a.length, var_j));
         var_b = 0;
         return 0;
-        var_e = 0x15361ae10614c1f1cd8c2fd2a53491eb95bbd15507a2515defc38ea226c54360;
-        var_f = 0x1a200a94c040aae7de7ecdf4773fff7f4be656e51b65957134332a0bd3efedfa;
-        var_g = arg9;
-        (bool success, bytes memory ret0) = address(0x07).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x15361ae10614c1f1cd8c2fd2a53491eb95bbd15507a2515defc38ea226c54360, 0x1a200a94c040aae7de7ecdf4773fff7f4be656e51b65957134332a0bd3efedfa, arg9));
-        var_b = 0;
-        return 0;
-        var_g = var_h;
-        var_i = var_j;
-        (bool success, bytes memory ret0) = address(0x06).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x15361ae10614c1f1cd8c2fd2a53491eb95bbd15507a2515defc38ea226c54360, 0x1a200a94c040aae7de7ecdf4773fff7f4be656e51b65957134332a0bd3efedfa, var_h, var_j));
-        var_b = 0;
-        return 0;
-        var_e = 0x0af34571f0b0f3f8e1935f2ed0508e4496fa01c57869f638f60a6c5f63d23643;
-        var_f = 0x256fe3487628807083293ff8193191fba9ea23b768d0fa5022dadc19d2fe50d7;
-        var_g = arg10;
-        (bool success, bytes memory ret0) = address(0x07).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(0x0af34571f0b0f3f8e1935f2ed0508e4496fa01c57869f638f60a6c5f63d23643, 0x256fe3487628807083293ff8193191fba9ea23b768d0fa5022dadc19d2fe50d7, arg10));
+        uint256 var_k = arg0;
+        uint256 var_l = (0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 - (arg1 % 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47)) % 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47;
+        uint256 var_m = arg2;
+        uint256 var_n = arg3;
+        uint256 var_o = arg4;
+        uint256 var_p = arg5;
+        var_q = 0x2d4d9aa7e302d9df41749d5507949d05dbea33fbb16c643b22f599a2be6df2e2;
+        var_r = 0x14bedd503c37ceb061d8ec60209fe345ce89830a19230301f076caff004d1926;
+        var_s = 0x0967032fcbf776d1afc985f88877f182d38480a653f2decaa9794cbc3bf3060c;
+        var_t = 0x0e187847ad4c798374d0d6732bf501847dd68bc0e071241e0213bc7fc13db7ab;
+        var_u = 0x304cfbd1e08a704a99f5e847d93f8c3caafddec46b7a0d379da69a4d112346a7;
+        var_v = 0x1739c1b1a457a8c7313123d24d2f9192f896b7c63eea05a9d57f06547ad0cec8;
+        var_w = var_x;
+        var_y = var_i;
+        var_z = 0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2;
+        uint256 var_aa = 0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed;
+        uint256 var_ab = 0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b;
+        uint256 var_ac = 0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa;
+        uint256 var_ad = arg6;
+        uint256 var_ae = arg7;
+        uint256 var_af = 0x2c78c6772d66334e80df349c572351ff9bb886cd29ffa24336538d96500bd3ef;
+        uint256 var_ag = 0x162f162900cea61e8df3a973044ddfb8183d254f537d717113397a278476121f;
+        uint256 var_ah = 0x1282b7945c387aa24f6808928e6a91ec1dc332c04f05b9e1439b7a79b245c661;
+        uint256 var_ai = 0x2024cf1d964db7854f708d5c5c4eea1660564a763ef5a68c3f86248eff1749db;
+        (bool success, bytes memory ret0) = address(0x08).{ gas: gasleft() - 0x07d0 }staticcall(abi.encode(arg0, (0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47 - (arg1 % 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47)) % 0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47, arg2, arg3, arg4, arg5, 0x2d4d9aa7e302d9df41749d5507949d05dbea33fbb16c643b22f599a2be6df2e2, 0x14bedd503c37ceb061d8ec60209fe345ce89830a19230301f076caff004d1926, 0x0967032fcbf776d1afc985f88877f182d38480a653f2decaa9794cbc3bf3060c, 0x0e187847ad4c798374d0d6732bf501847dd68bc0e071241e0213bc7fc13db7ab, 0x304cfbd1e08a704a99f5e847d93f8c3caafddec46b7a0d379da69a4d112346a7, 0x1739c1b1a457a8c7313123d24d2f9192f896b7c63eea05a9d57f06547ad0cec8, var_x, var_i, 0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2, 0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed, 0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b, 0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa, arg6, arg7, 0x2c78c6772d66334e80df349c572351ff9bb886cd29ffa24336538d96500bd3ef, 0x162f162900cea61e8df3a973044ddfb8183d254f537d717113397a278476121f, 0x1282b7945c387aa24f6808928e6a91ec1dc332c04f05b9e1439b7a79b245c661, 0x2024cf1d964db7854f708d5c5c4eea1660564a763ef5a68c3f86248eff1749db));
+        var_b = (var_aj) & (success);
+        return (var_aj) & (success);
     }
 }
